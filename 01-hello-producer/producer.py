67269aebf63f704b8ec44357af7829374080cdf2
Prashant Kumar Pandey <LearningJournal@users.noreply.github.com>
2021-04-02 13:47:30 +0530

    Initial Commit

--- 01-hello-producer/producer.py
+++ 01-hello-producer/producer.py
@@ -1,17 +1,20 @@
 from confluent_kafka import Producer
+import socket
 
 if __name__ == '__main__':
     print("Starting Kafka Producer")
 
-    topic = 'hello-producer'
-    producer_config = {'bootstrap.servers': 'localhost:9092'}
+    producer_config = {'client.id': socket.gethostname(),
+                       'bootstrap.servers': 'localhost:9092'}
 
     print("Creating Producer")
     producer = Producer(producer_config)
 
     print("Producing Kafka Message")
-    for i in range(100000):
-        producer.produce(topic, key=str(i), value="Simple Message-" + str(i))
+    for i in range(1, 101):
+        for j in range(1, 10001):
+            producer.produce('hello-producer', key=str(j*i), value="Simple Message-" + str(j*i))
+        producer.poll()
 
     producer.flush()
     print("Finished Kafka Producer")
