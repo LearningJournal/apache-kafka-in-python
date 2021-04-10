1feda568aa46e47bb3e6bf65b6f74bb986961830
Tasneem <uknow2009@gmail.com>
2021-04-10 18:41:05 +0530

    producer with delivery status

--- 01-hello-producer/producer_with_delivery.py
+++ 01-hello-producer/producer_with_delivery.py
@@ -7,31 +7,21 @@
 #
 # =============================================================================
 
-import logging
 import socket
-import sys
 
 from confluent_kafka import Producer
 
-LOG = logging.getLogger('learning_journal')
-LOG.setLevel(logging.DEBUG)
-BASE_FMT = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                             datefmt='%Y-%m-%d %H:%M:%S')
-
-console_out = logging.StreamHandler(sys.stdout)
-console_out.setFormatter(BASE_FMT)
-LOG.addHandler(console_out)
 delivered_records = 0
 
 
-def acked(err, msg1):
+def callback(err, msg):
     global delivered_records
     if err is not None:
-        LOG.error("Failed to deliver message: {}".format(err))
+        print("Failed to deliver message: {}".format(err))
     else:
         delivered_records += 1
-        LOG.info("Produced record to topic {} partition [{}] @ offset {}"
-                 .format(msg1.topic(), msg1.partition(), msg1.offset()))
+        print("Produced record to topic {} partition [{}] @ offset {}"
+              .format(msg.topic(), msg.partition(), msg.offset()))
 
 
 if __name__ == '__main__':
@@ -47,9 +37,9 @@ if __name__ == '__main__':
     for i in range(1, 101):
         for j in range(1, 11):
             producer.produce('hello-producer', key=str(j * i), value="Simple Message-" + str(j * i),
-                             on_delivery=acked)
+                             on_delivery=callback)
         producer.poll()
 
     producer.flush()
-    print("{} messages were produced to topic {}!".format(delivered_records, 'hello-producer'))
+    print("{} messages were delivered to topic {}!".format(delivered_records, 'hello-producer'))
     print("Finished Kafka Producer")
