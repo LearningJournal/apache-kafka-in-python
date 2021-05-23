222f13ed615a8d7536f3d94d4083b16a457ff53f
Tasneem <uknow2009@gmail.com>
2021-05-23 17:12:48 +0530

    consumer manual commit (sync/async)

--- 02-hello-consumer/kafka-scripts.sh
+++ 02-hello-consumer/kafka-scripts.sh
@@ -5,4 +5,6 @@ kafka-topics --create --zookeeper localhost:2181 --replication-factor 1 --partit
 
 kafka-console-producer --bootstrap-server localhost:9092 --topic hello-consumer
 
+kafka-consumer-groups --bootstrap-server localhost:9092 --group learning_journal --describe
+
 confluent local destroy
